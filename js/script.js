813f124506fe4a1f05fc0c604d9d147fc96dcad9
erikagrotti <erikagrotti2014@gmail.com>
2024-07-30 16:15:17 -0300

    fiz(scroll): edit scroll

--- js/script.js
+++ js/script.js
@@ -109,7 +109,46 @@ $(document).ready(function(){
                 } else {
                     $(this).fadeIn();
                 }
-        });
-    }}
+            });
+        }
+    }
+
+    // scroll para seções 
+    let navBtn = $('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let servicesSection = $('#service-area');
+    let teamSection = $('#team-area');
+    let portifolioSection = $('#portifolio-area');
+    let contactSection = $('#contact-area');
+
+    let scrollTo = '';
+
+    $(navBtn).click(function(){
+
+        let btnId = $(this).attr('id');
+
+        console.log(btnId);
+
+        if(btnId == 'about-menu'){
+            scrollTo = aboutSection;
+        } else if(btnId == 'service-menu'){
+            scrollTo = servicesSection;
+        } else if(btnId == 'team-menu'){
+            scrollTo = teamSection;
+        } else if(btnId == 'portfolio-menu'){
+            scrollTo = portifolioSection;
+        } else if(btnId == 'contact-menu'){
+            scrollTo = contactSection;
+        } else{
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(scrollTo).offset().top - 70
+        }, 1500)
+    })
+
 
 });
